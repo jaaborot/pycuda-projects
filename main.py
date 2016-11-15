786a9ab718b7a0f6957fa3faf1d5be8d9feb7d3f
jaaborot <jaaborot@up.edu.ph>
2016-11-15 15:33:56 +0800

    Added blank main.py to source code.

---
+++ main.py
@@ -0,0 +1,8 @@
+###########################################################
+# Author: Jeffrey A. Aborot                               #
+# Institution: Algorithms & Complexity Laboratory         #
+#              Department of Computer Science             #
+#              University of the Philippines Diliman      #
+###########################################################
+
+print 'Booyah!'
